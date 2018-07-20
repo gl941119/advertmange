7b61a1924177ddc0d8bd39c391f184d56540ce62
yuqian <yuqian@bierinc.com>
2018-07-20 20:49:24 +0800

    production

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,7 @@
 const url = {
     localTestUrl: 'http://192.168.1.15:8092/',
-    productUrl: 'http://192.168.1.254:8092/'
+    productUrl: 'http://192.168.1.254:8092/',
+    // productUrl: '/mapi/',
 };
 
 export default {
@@ -9,5 +10,5 @@ export default {
     miniPageSize: 10, // 小量
     pageStart: 1,
     UploadBulletinFileUrl: (process.env.NODE_ENV === 'development' ? url.localTestUrl : url.productUrl) + 'amazonsuper/pagehome/uploadfile',
-    UploadImg: (process.env.NODE_ENV === 'development' ? url.localTestUrl : url.productUrl) + 'advertmange/common/upload-file'
+    UploadImg: (process.env.NODE_ENV === 'development' ? url.localTestUrl : url.productUrl) + 'advertmange/common/upload-file',
 };
