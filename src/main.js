fcc9e91a1fc1c74c6e48b68927e4e05132349f70
gaigai <gaigaiyang@bierinc.com>
2018-09-01 18:04:05 +0800

    main

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import App from './APP';
+import App from './App';
 import router from './router';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css'; // 引入element-ui的样式
